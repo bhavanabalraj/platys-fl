58a458f50803b2aefa897e6cf9624166ec5b3393
Bhavana Balraj <ceg.bhavana@gmail.com>
2020-09-30 23:38:20 -0400

    FLearning_HAR

---
+++ Active Learning/FLearning.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 22:20:07 2020
+
+@author: cegbh
+"""
+
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch.nn.functional as F
+from torch import optim
+import copy
+
+xHarData = pd.read_csv('UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt', header=None)
+yHarData = pd.read_csv('UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt', header=None)
+
+def transform(x):
+ 
+    x = (x.split(' '))
+    ans = [] 
+    
+    for i, data in enumerate(x):
+        if not data == '':
+            ans.append(float(data))
+       
+    return ans
+
+
+newTrainData =[]
+
+for i in range(len(xHarData[0])):
+    newTrainData.append(transform(xHarData[0][i]))
+
+
+X_train = torch.tensor(newTrainData)
+y_train = torch.tensor(yHarData[0].values).flatten()
+
+X_testData = pd.read_csv('UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt', header=None)
+y_testData = pd.read_csv('UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt', header=None)
+
+newTestData = []
+
+for i in range(len(X_testData[0])):
+    newTestData.append((transform(X_testData[0][i])))
+
+X_test = torch.tensor(newTestData)
+y_test = torch.tensor(y_testData[0].values).flatten()
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Inputs to hidden layer linear transformation
+        self.h1 = nn.Linear(561, 1000)
+        self.h2 = nn.Linear(1000, 500)
+              
+        # Output layer, 10 units - one for each digit
+        self.output = nn.Linear(500, 7)
+        
+    def forward(self, x):
+               
+        # Hidden layer with sigmoid activation
+        x = F.dropout(F.relu(self.h1(x)))
+        x = F.dropout(F.relu(self.h2(x)))
+        
+        # Output layer with softmax activation
+        x = F.softmax(self.output(x), dim=1)
+        
+        return x
+
+
+model = Network()
+w_glob = model.state_dict()
+
+X_split_dataset = torch.split(X_train,1000)
+y_split_dataset = torch.split(y_train,1000)
+
+
+def train(net, X_train, y_train):
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.003)
+    
+    epochs = 50
+    train_losses, test_losses = [], []
+    batch_size = 32
+    
+    trainloader = len(X_train)/batch_size
+    testloader = len(X_test)/batch_size
+    
+    
+    for e in range(epochs):
+        
+        running_loss = 0 
+        
+        permutation = torch.randperm(X_train.size()[0])
+        
+        for i in range(0, X_train.size()[0], batch_size):
+            
+            optimizer.zero_grad()
+            
+            indices = permutation[i:i+batch_size]
+            batch_x, batch_y = X_train[indices], y_train[indices]
+            
+            log_ps = net(batch_x)
+            loss = criterion(log_ps, batch_y)
+            loss.backward()
+            optimizer.step()
+        
+            running_loss += loss.item()
+            
+        else:
+            
+            test_loss = 0
+            accuracy = 0
+            
+            permutation = torch.randperm(X_test.size()[0])
+        
+            for i in range(0, X_test.size()[0], batch_size):
+            
+                # Turn off gradients for validation, saves memory and computations
+                with torch.no_grad():
+                    
+                    net.eval()
+                    indices = permutation[i:i+batch_size]
+                    batch_x, batch_y = X_test[indices], y_test[indices]
+                    log_ps = net(batch_x)
+                    test_loss += criterion(log_ps, batch_y)
+                    
+                    ps = torch.exp(log_ps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == batch_y.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor))
+                          
+            net.train()
+            
+            train_losses.append(running_loss/trainloader)
+            test_losses.append(test_loss/testloader)
+        
+            print("Epoch: {}/{}.. ".format(e+1, epochs),
+                  "Training Loss: {:.3f}.. ".format(running_loss/trainloader),
+                  "Test Loss: {:.3f}.. ".format(test_loss/testloader),
+                  "Test Accuracy: {:.3f}".format(accuracy/testloader))
+        
+    
+        plt.plot(train_losses)
+        plt.plot(test_losses)
+    
+    return net.state_dict()
+
+#train(model)
+
+def FedAvg(w):
+    w_avg = copy.deepcopy(w[0])
+    for k in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[k] += w[i][k]
+        w_avg[k] = torch.div(w_avg[k], len(w))
+    return w_avg
+
+m = max(int(0.5 * 8), 1)
+idxs_users = np.random.choice(range(8), m, replace=False)
+
+for iterations in range(2):
+    w_locals = []
+    for idx in idxs_users:
+       print("USER:", idx)
+       net=copy.deepcopy(model)
+       w = train(net, X_split_dataset[idx], y_split_dataset[idx])
+       if idx == idxs_users[0]: plt.show()
+       w_locals.append(copy.deepcopy(w))
+       #loss_locals.append(copy.deepcopy(loss))
+       # update global weights
+    
+    w_glob = FedAvg(w_locals)
+    
+    # copy weight to net_glob
+    model.load_state_dict(w_glob)
+
+print("--------AFTER AVG--------")
+
+plt.clf()
+
+for idx in idxs_users:
+   print("USER:", idx)
+   net=copy.deepcopy(model)
+   w = train(net, X_split_dataset[idx], y_split_dataset[idx])
+   plt.show()
+   w_locals.append(copy.deepcopy(w))
+
+
+
+
+
